bebbdbf1452f27a84a9acf1b51275854bd3cbcbd
rvd <nseries_nokia@list.ru>
2025-05-31 18:27:18 +0400

    testCommit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int myInt = 40000;
+        byte myByte = 22;
+        short myShort = 32000;
+        long myLong = 100000L;
+        float myFloat = 3.75f;
+        double myDouble = 12.3456789;
+
+        System.out.println("Значение переменной myInt с типом int равно " + myInt);
+        System.out.println("Значение переменной myByte с типом byte равно " + myByte);
+        System.out.println("Значение переменной myShort с типом short равно " + myShort);
+        System.out.println("Значение переменной myLong с типом long равно " + myLong);
+        System.out.println("Значение переменной myFloat с типом float равно " + myFloat);
+        System.out.println("Значение переменной myDouble с типом double равно " + myDouble);
+    }
+}
