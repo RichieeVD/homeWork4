f38c8bede115c05b23d69280ea48b73aa64b3f1e
rvd <nseries_nokia@list.ru>
2025-06-02 15:02:11 +0400

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,27 @@ public class Main {
         float myFloat = 3.75f;
         double myDouble = 12.3456789;
 
-        System.out.println("Значение переменной myInt с типом int равно" + myInt);
+        System.out.println("Значение переменной myInt с типом int равно " + myInt);
         System.out.println("Значение переменной myByte с типом byte равно " + myByte);
         System.out.println("Значение переменной myShort с типом short равно " + myShort);
         System.out.println("Значение переменной myLong с типом long равно " + myLong);
         System.out.println("Значение переменной myFloat с типом float равно " + myFloat);
         System.out.println("Значение переменной myDouble с типом double равно " + myDouble);
+
+        double valueDouble = 27.12;
+        long  bigLong = 987678965549L;
+        float valueFloat = 2.786f;
+        short valueShort = 569;
+        int negativeInt = -159;
+        short anotherShort = 27897;
+        byte smallByte = 67;
+
+        System.out.println("Значение переменной valueDouble с типом double равно " + valueDouble);
+        System.out.println("Значение переменной bigLong с типом long равно " + bigLong);
+        System.out.println("Значение переменной valueFloat с типом float равно " + valueFloat);
+        System.out.println("Значение переменной valueShort с типом short равно " + valueShort);
+        System.out.println("Значение переменной negativeByte с типом byte равно " + negativeInt);
+        System.out.println("Значение переменной anotherShort с типом short равно " + anotherShort);
+        System.out.println("Значение переменной smallByte с типом byte равно " + smallByte);
     }
 }
